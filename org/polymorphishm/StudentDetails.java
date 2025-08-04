37121d1d474ecb3a6a50a201b1b8d135c23fc6ca
Priyanka199522 <priyavictor22@gmail.com>
2025-08-04 14:45:56 +0530

    new code added

--- org/polymorphishm/StudentDetails.java
+++ org/polymorphishm/StudentDetails.java
@@ -3,31 +3,34 @@ package org.polymorphishm;
 public class StudentDetails {
 
 	private void studentName() {
-		System.out.println("priya");
+		System.out.println("Student information");
 	}
 	private void studentName(String Name) {
-		System.out.println("StudentName is: "+"Pushpa");
+		System.out.println("StudentName is: "+"name");
 	
+}private void studentName(float weight) {
+	System.out.println("Student weight is: "+weight);
 }
+
 	private void studentName(int age) {
-		System.out.println("Student Age is: "+24);
+		System.out.println("Student Age is: "+age);
 	}
 	private void studentName(long phonenumber,String email) {
 
-		System.out.println("phone nmber is: "+9466646466l);
+		System.out.println("phone nmber is: "+phonenumber);
 	
-		System.out.println("email is: "+"priyavictor22@gmail.com");
+		System.out.println("email is: "+email);
 	}
 	private void studentName(String Address,int pincode) {
-		System.out.println("address: "+"kakithapuram" );
-		System.out.println(" pincode is :"+6275033);
+		System.out.println("address: "+Address );
+		System.out.println(" pincode is :"+pincode);
 	}
 
 	
 	public static void main(String[] args) {
 	 StudentDetails S=new StudentDetails();
 	 S.studentName();
-	 S.studentName("PHUPHA");
+	 S.studentName("Karish");
 	 S.studentName(74676764773L, "ramyapriya@gmail.com");
 	 S.studentName("rajapuram" , 6444454);
 	}
